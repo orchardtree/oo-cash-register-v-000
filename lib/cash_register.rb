d844abd3c70d9a83a05620b8f00ba7c5f4d5acd0
Jesse Wenger <wengerj@gmail.com>
2021-03-08 20:46:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
-
+require 'pry'
+class CashRegister
+  attr_accessor :discount, :total
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(product_title, product_price, quantity = 1)
+    @total += product_price * quantity
+  end
+  
+  def apply_discount
+    @total = @total * (@discount/100)
+    binding.pry
+  end
+  
+end
